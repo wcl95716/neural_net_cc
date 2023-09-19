c82d9751892056caa2e45943b333eea62f1cc4d9
Panda <panda@MacBook-Pro.local>
2023-09-19 22:50:42 +0800

    update

--- base/base_template/test/test_lib.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "gtest/gtest.h"
-#include "../lib.h"
-
-int main(int argc, char **argv) {
-    SetupUserRoutes();
-    svr.listen("0.0.0.0", 8080);
-	//return RUN_ALL_TESTS();
-}
--- base/server/lib.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Panda on 2021/6/21.
-//
-
-#include "lib.h"
-using std::swap;
-
-httplib::Server svr;
