ef165a41c23fd738c652d4f245e58a3f32024300
Panda <panda@MacBook-Pro.local>
2023-09-21 19:46:28 +0800

    update

--- modules/server/lib.cpp
+++ modules/server/lib.cpp
@@ -1,8 +1,56 @@
 //
-// Created by Panda on 2021/6/21.
+// Created by Panda on 2023/9/18.
 //
 
 #include "lib.h"
 using std::swap;
 
 httplib::Server svr;
+
+// 构造函数实现
+HttpServer::HttpServer() {
+    svr.Get("/hi", [](const httplib::Request&, httplib::Response& res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+}
+
+// 获取实例方法实现
+HttpServer& HttpServer::getInstance() {
+    static HttpServer instance;
+    return instance;
+}
+
+// 添加路由方法实现
+bool HttpServer::addRoute(HttpMethod method, const char* path, const httplib::Server::Handler& handler) {
+    std::unique_lock<std::mutex> lock(mtx);
+
+    if (registeredPaths.find(path) != registeredPaths.end()) {
+        return false;
+    }
+
+    switch (method) {
+        case GET:
+            svr.Get(path, handler);
+            break;
+        case POST:
+            svr.Post(path, handler);
+            break;
+        case PUT:
+            svr.Put(path, handler);
+            break;
+        case DELETE:
+            svr.Delete(path, handler);
+            break;
+        // ...可以为其他HTTP方法添加相应的case
+        default:
+            return false;  // 不支持的HTTP方法
+    }
+
+    registeredPaths.insert(path);
+    return true;
+}
+
+// 监听方法实现
+void HttpServer::listen(const char* host, int port) {
+    svr.listen(host, port);
+}
--- src/template/test/test_lib.cpp
+++ src/template/test/test_lib.cpp
@@ -19,22 +19,12 @@ TEST(TestCase, test3) {
     EXPECT_EQ(3, add(1, 2));
 }
 
-void SetupUserRoutes() {
-    svr.Get("/", [](const httplib::Request&, httplib::Response& res) {
-        res.set_content("Get user information  111111  ", "text/plain");
-    });
-    svr.Get("/test", [](const httplib::Request&, httplib::Response& res) {
-        res.set_content(" 22222  ", "text/plain");
-    });
-    // ... 其他User模块的接
-}
+
 
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     BasicHead basic_head;
     basic_head.addNum(2);
     std::cout << basic_head.getTop() << std::endl;
-     SetupUserRoutes();
-     svr.listen("0.0.0.0", 8080);
     return RUN_ALL_TESTS();
 }
